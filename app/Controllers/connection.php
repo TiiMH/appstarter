6dc83b72c4c1ed37a69334abc87b960d0c83bd6d
Razafindrakoto Nathan <Nathan.Raza18@gmail.com>
2024-04-08 10:50:35 +0200

    10.50

---
+++ app/Controllers/connection.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Controllers;
+class Connection extends BaseController
+{
+    public function index(): string
+    {
+        // A COMPLETER
+        return "Hello world ! /login fonctionne";
+    }
+}
